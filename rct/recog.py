567c094905280cffe72dde5ce375ce534e836b17
mmkuznecov <43701555+mmkuznecov@users.noreply.github.com>
2019-01-29 02:17:00 +0300

    Update recog.py

--- rct/recog.py
+++ rct/recog.py
@@ -77,140 +77,84 @@ class Recognition:
             status = "Waiting"
             rects = []
             if totalFrames % sk_fr == 0:
-                
                 status = "Detecting"
                 trackers = []
-
-                
                 blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
                 net.setInput(blob)
                 detections = net.forward()
-
-                
                 for i in np.arange(0, detections.shape[2]):
-                    
                     confidence = detections[0, 0, i, 2]
                     if confidence > conf:
-                        
                         idx = int(detections[0, 0, i, 1])
-
-                        
                         if CLASSES[idx] != obj:
                             continue
-
-                        
                         box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
                         (startX, startY, endX, endY) = box.astype("int")
-
-                        
                         tracker = dlib.correlation_tracker()
                         rect = dlib.rectangle(startX, startY, endX, endY)
                         tracker.start_track(rgb, rect)
-
-                        
                         trackers.append(tracker)
-
-            
             else:
-                
                 for tracker in trackers:
-                    
                     status = "Tracking"
-
-                    
                     tracker.update(rgb)
                     pos = tracker.get_position()
-
                     startX = int(pos.left())
                     startY = int(pos.top())
                     endX = int(pos.right())
                     endY = int(pos.bottom())
-
                     rects.append((startX, startY, endX, endY))
             #cv2.line(frame, (0, H // 2), (W, H // 2), (0, 255, 255), 2)
             objects = ct.update(rects)
-
             for (objectID, centroid) in objects.items():
-                
                 to = trackableObjects.get(objectID, None)
-
-                
                 if to is None:
                     to = TrackableObject(objectID, centroid)
-
-                
-                
                 else:
-                    
                     y = [c[1] for c in to.centroids]
                     direction = centroid[1] - np.mean(y)
                     to.centroids.append(centroid)
-
-                    
                     if not to.counted:
-                        
                         if direction < 0 and centroid[1] < H // 2:
                             #totalUp += 1
                             total += 1
                             to.counted = True
-
-                        
                         elif direction > 0 and centroid[1] > H // 2:
                             #totalDown += 1
                             total += 1
                             to.counted = True
-
-                
                 trackableObjects[objectID] = to
-
-                
                 text = "ID {}".format(objectID)
                 cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
                 cv2.circle(frame, (centroid[0], centroid[1]), 4, (255, 255, 255), -1)
-
-            
             info = [
                 #("Up", totalUp),
                 #("Down", totalDown),
                 ('Total',total),
                 ("Status", status),
             ]
-
-            
             for (i, (k, v)) in enumerate(info):
                 text = "{}: {}".format(k, v)
                 #cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
                     #cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-
-            
             if writer is not None:
                 writer.write(frame)
-
-            
             cv2.imshow("Frame", frame)
             key = cv2.waitKey(1) & 0xFF
 
             # if the `q` key was pressed, break from the loop
             if key == ord("q"):
                 break
-
-            
             totalFrames += 1
             fps.update()
-
         fps.stop()
         print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
         print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-
-        
         if writer is not None:
             writer.release()
-
-        
         else:
             vs.release()
-
         cv2.destroyAllWindows()
 
 
