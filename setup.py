65c1a260603b933808b851d8b6ac5b8c46033521
mmkuznecov <43701555+mmkuznecov@users.noreply.github.com>
2019-01-28 22:34:14 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,5 @@ from os.path import join, dirname
 setup(
     name='rct',
     version='1.0',
-    packages= ['rct'],
-    long_description=open(join(dirname(__file__), 'README.txt')).read()
+    packages= ['rct']
     )
